d584b9510f97069bbce8bfee7e776eba87c600b2
leonel <leonelhcontreras@gmail.com>
2022-11-13 23:01:38 -0400

    se agrego el eslint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
   return (
-   <h1 className='text-center text-blue-900 flex justify-center items-center'>Empezamos</h1>
-  )
+    <h1 className="text-center text-blue-900 flex justify-center items-center">
+      Empezamos
+    </h1>
+  );
 }
 
-export default App
+export default App;
