51a935cc134fa4c4b0b983e4099b2d5e42a7b313
leonel <leonelhcontreras@gmail.com>
2022-11-14 23:25:56 -0400

    [ADD]:se agrego los endpoint de login y registro de usaurio

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,18 +1,16 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 
-const connectDB= async()=>{
-    console.log('prueba')
-    //Hay que crear una cuenta en MongoDB para hacer la conexion
-    // try {
-    //     const connection=await mongoose.connect(process.env.MONGO_URI,{
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology:true, 
-    //     })
-    //     const url=`${connection.connection.host}:${connection.connection.port}`
-    //     console.log(`MongoDB Conectado en ${url} `)
-    // } catch (error) {
-    //     console.log(`error: ${error.message}`)
-    // }
+const connectDB= async():Promise<void>=>{
+    try {
+        const connection= await mongoose.connect(<string>process.env.MONGO_URI,{
+                useNewUrlParser: true,
+                useUnifiedTopology: true  
+        } as ConnectOptions)
+        const url=`${connection.connection.host}:${connection.connection.port}`
+        console.log(`MongoDB Conectado en ${url} `)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default connectDB
---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,50 @@
+import {Request,Response} from 'express';
+import User from '../models/user';
+import createJWT from '../helpers/generateJWT';
+import generateId from '../helpers/generateID';
+ 
+
+//Permite al usuario hacer login y entrar al sistema
+export const login = async (req: Request, res: Response):Promise<Response | undefined> => {
+    try {
+       
+        const { email, password, } = req.body
+        const user = await User.findOne({ email })
+       
+        //Verificar si ya existe el usuario 
+        if (!user) {
+            return res.json({msg:"The User does not exists", email:false})
+        }
+        //Comprobar su password
+        if (await user?.comparePassword(password)) {
+          //Retorna todos los datos del usuario , el token y un booleano para validar que logueo correctamente
+          return res.json({ 
+            ...user?.toJSON(),
+            token:createJWT(user?._id),
+            login: true
+           })
+        }
+      return res.json({msg: "The password is wrong", login: false })
+
+     
+      } catch (error) {
+        console.log(error)
+      }
+
+}
+
+//Permite registrar un nuevo Usuario
+export const register = async (req: Request, res: Response):Promise<Response | undefined> => {
+    //Evitar registros duplicados
+    const {email}=req.body
+    const user = await User.findOne({email})
+    if (user) {
+        return res.json({msg:"The User already Exists", register:false})
+    }
+
+    const newUser = new User(req.body)
+    newUser.token= generateId()
+    await newUser.save()
+
+    return res.json({msg:"User created", register:true})
+}
