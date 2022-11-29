edefb2631144d087c06f5b2093c270600cabe5bf
Daniel1264 <94574017+Daniel1264@users.noreply.github.com>
2022-11-29 08:56:44 -0600

    se creo el componente para ti

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
-import { Routes, Route } from "react-router-dom";
-import Registro from "./pages/Registro";
-import Home from "./components/Home";
-import Login from "./pages/Login";
-import NewPassword from "./pages/NewPassword";
-import Protected from "./components/layout/Protected";
-import { AuthProvider } from "./context/AauthProvider";
-import ForgetPassword from "./components/ForgetPassword";
-import ConfirmAccount from "./pages/ConfirmAccount";
-import BeforeStart from "./components/BeforeStart";
-import Targets from "./components/Targets";
-import PaidPackage from "./pages/PaidPackage";
+import { Routes, Route } from 'react-router-dom';
+import Registro from './pages/Registro';
+import Home from './components/Home';
+import Login from './pages/Login';
+import NewPassword from './pages/NewPassword';
+import Protected from './components/layout/Protected';
+import { AuthProvider } from './context/AauthProvider';
+import ForgetPassword from './components/ForgetPassword';
+import ConfirmAccount from './pages/ConfirmAccount';
+import BeforeStart from './components/BeforeStart';
+import Targets from './components/Targets';
+import PaidPackage from './pages/PaidPackage';
+import ForYou from './pages/ForYou';
+import TerapistPanel from './pages/TerapistPanel';
 
 function App() {
   return (
     <AuthProvider>
-      <div className="bg-primary min-h-screen flex items-center justify-center">
+      <div className="bg-primary min-h-screen w-full flex items-center justify-center">
         {/* Rutas Publicas */}
         <Routes>
           <Route path="/register" element={<Registro />} />
@@ -28,6 +30,8 @@ function App() {
           <Route path="/" element={<Protected />}>
             <Route path="/home" element={<Home />} />
             <Route path="/paid" element={<PaidPackage />} />
+            <Route path="/you" element={<ForYou />} />
+            <Route path="/controler" element={<TerapistPanel />} />
           </Route>
         </Routes>
       </div>
--- frontend/src/components/layout/ModalFeelings.jsx
+++ frontend/src/components/layout/ModalFeelings.jsx
@@ -29,9 +29,12 @@ function ModalFeelings({ handleModal }) {
   ];
 
   return (
-    <div className="flex flex-col bg-fifth p-3 gap-3 z-10  fixed justify-center rounded-lg m-2">
+    <div className="flex flex-col bg-fifth p-3 gap-3  lg:w-1/3 fixed justify-center items-center rounded-lg m-2">
       <div className="absolute top-1 right-1">
-        <AiFillCloseCircle onClick={handleModal} className="text-2xl text-third cursor-pointer" />
+        <AiFillCloseCircle
+          onClick={handleModal}
+          className="text-2xl text-third cursor-pointer"
+        />
       </div>
       <div>
         <h2 className="text-fourth text-center text-2xl mt-4">
@@ -52,7 +55,11 @@ function ModalFeelings({ handleModal }) {
         </div>
         <div className="flex gap-3 flex-wrap justify-center">
           {numberArrFeelings.map((button) => (
-            <button key={button} type="submit" className="p-2 rounded-md text-fourth bg-third">
+            <button
+              key={button}
+              type="submit"
+              className="p-2 rounded-md text-fourth bg-third"
+            >
               {fellingsOptions[button - 1]}
             </button>
           ))}
@@ -64,7 +71,7 @@ function ModalFeelings({ handleModal }) {
         </div>
         <div>
           <input
-            className="w-full bg-secondary rounded-lg p-1 text-seventh"
+            className="w-full bg-secondary rounded-lg p-1 outline-none text-seventh"
             type="text"
             name="search"
             placeholder="buscar mas tags"
@@ -72,14 +79,21 @@ function ModalFeelings({ handleModal }) {
         </div>
         <div className="flex gap-3 flex-wrap justify-center">
           {numberArrFeelingsTwo.map((button) => (
-            <button key={button} type="submit" className="p-2 rounded-md text-fourth bg-third">
+            <button
+              key={button}
+              type="submit"
+              className="p-2 rounded-md text-fourth bg-third"
+            >
               {fellingsOptionsTwo[button - 1]}
             </button>
           ))}
         </div>
       </div>
       <div className="ml-auto p-1">
-        <button type="submit" className="bg-third p-1 rounded-lg text-fourth w-24">
+        <button
+          type="submit"
+          className="bg-third p-1 rounded-lg text-fourth w-24"
+        >
           Continuar
         </button>
       </div>
