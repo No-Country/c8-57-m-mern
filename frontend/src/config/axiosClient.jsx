2d3a77b4d0e3b830df7388c42e56069278f714ac
Juan Ignacio <93682709+juani029@users.noreply.github.com>
2022-12-06 20:42:43 -0300

    change url axios

--- frontend/src/config/axiosClient.jsx
+++ frontend/src/config/axiosClient.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const deployUrl = 'https://back-fame-app.up.railway.app/';
+const localhost = 'http://localhost:4000/';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: deployUrl || localhost,
 });
 
 export default axiosClient;
